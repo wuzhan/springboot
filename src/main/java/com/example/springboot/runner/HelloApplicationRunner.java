edb82e5754bd4108cf0e8d7e0c4c3286d43696a2
wuzhan <wuzhan@asiainfo.com>
2020-12-11 09:41:45 +0800

    初始化项目

--- src/main/java/com/example/springboot/runner/HelloApplicationRunner.java
+++ src/main/java/com/example/springboot/runner/HelloApplicationRunner.java
@@ -3,7 +3,6 @@ package com.example.springboot.runner;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
 /**
  * @Auther: 13213
  * @Date: 2020/12/4 17:55
